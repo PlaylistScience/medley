981ebe3ec2fe3c6604d444fcf87745b579706ac6
Martin Stuessy <accounts@martinstuessy.com>
2018-12-30 17:40:21 -0500

    started integrating youtube iframe api into the vue component

--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -11,13 +11,10 @@ require('../scss/app.scss');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
-let testString: string = 'Typescript loaded';
-console.log(testString);
-
 import Vue from 'vue';
 import Example from './components/Example';
 
 new Vue({
-    el: '#app',
+    el: '#player',
     render: h => h(Example)
 });
--- assets/ts/components/Example.vue
+++ assets/ts/components/Example.vue
@@ -1,12 +1,103 @@
 <template>
     <div>
-        <h1>Medley</h1>
-        <p>Testing automatic transpilation</p>
+        <!-- The part that the youtube iframe api hooks into -->
+        <div id="player"></div>
+
     </div>
 </template>
 
 <script lang="ts">
     export default {
+        data: function () {
+            return {
+                player: null,
+                // done: false,
+            }
+        },
+        methods: {
+            onYouTubeIframeAPIReady() {
+                console.log('onYouTubeIframeAPIReady() vue function called');
 
+                this.player = new YT.Player('player', {
+                    height: '390',
+                    width: '640',
+                    videoId: 'M7lc1UVf-VE',
+                    events: {
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange
+                    }
+                });
+            },
+            onPlayerReady(event) {
+                console.log('onPlayerReady function');
+                event.target.playVideo();
+            },
+            onPlayerStateChange() {
+                console.log('onPlayerStateChange function');
+            },
+            stopVideo() {
+                console.log('stopVideo function');
+            }
+        },
+        created: function() {
+        //     this.loadYoutube();
+
+        //     // map expected youtube function names to vue names
+        //     function onPlayerReady() {
+        //         console.log('onPlayerReady function called by youtube api');
+        //     }
+
+        //     // function onYouTubeIframeAPIReady() {
+        //     //     console.log('youtube ready function called');
+        //     //     this.onYouTubeIframeAPIReady();
+        //     // }
+
+            var tag = document.createElement('script');
+
+            tag.src = "https://www.youtube.com/iframe_api";
+            var firstScriptTag = document.getElementsByTagName('script')[0];
+            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+            // 3. This function creates an <iframe> (and YouTube player)
+            //    after the API code downloads.
+            var player;
+
+            window.onYouTubeIframeAPIReady = this.onYouTubeIframeAPIReady;
+
+            // window.onYouTubeIframeAPIReady = function() {
+            //     player = new YT.Player('player', {
+            //         height: '390',
+            //         width: '640',
+            //         videoId: 'M7lc1UVf-VE',
+            //         events: {
+            //         'onReady': onPlayerReady,
+            //         'onStateChange': onPlayerStateChange
+            //         }
+            //     });
+            // }
+
+            // 4. The API will call this function when the video player is ready.
+            window.onPlayerReady = this.onPlayerReady;
+            // window.onPlayerReady = function(event) {
+            //     event.target.playVideo();
+            // }
+
+            // 5. The API calls this function when the player's state changes.
+            //    The function indicates that when playing a video (state=1),
+            //    the player should play for six seconds and then stop.
+            var done = false;
+            window.onPlayerStateChange = this.onPlayerStateChange;
+            // window.onPlayerStateChange = function(event) {
+            //     if (event.data == YT.PlayerState.PLAYING && !done) {
+            //         setTimeout(stopVideo, 6000);
+            //         done = true;
+            //     }
+            // }
+
+            window.stopVideo = this.stopVideo;
+            // window.stopVideo = function() {
+            //     player.stopVideo();
+            // }
+        }
     }
 </script>
