31e056a76844b9b43c4177f5d69790bdd0e1e3de
Martin Stuessy <martins@xigen.co.uk>
2019-01-10 13:14:16 +0000

    Import script, create Track entities from ApiTrack object -- still need to add genres

--- src/Migrations/Version20190110123535.php
+++ src/Migrations/Version20190110123535.php
@@ -8,14 +8,15 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190108232817 extends AbstractMigration
+final class Version20190110123535 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
-        $this->addSql('ALTER TABLE track ADD created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL');
+        $this->addSql('CREATE TABLE track (id INT NOT NULL, name VARCHAR(255) NOT NULL, artist VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE track_genre ADD CONSTRAINT FK_F3A7915F5ED23C43 FOREIGN KEY (track_id) REFERENCES track (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema) : void
@@ -24,6 +25,7 @@ final class Version20190108232817 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE track DROP created_at');
+        $this->addSql('ALTER TABLE track_genre DROP CONSTRAINT FK_F3A7915F5ED23C43');
+        $this->addSql('DROP TABLE track');
     }
 }
