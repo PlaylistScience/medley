ca725794dd3edd0ba0265cac69518065159ee3a7
Martin Stuessy <accounts@martinstuessy.com>
2019-01-20 15:38:00 +0000

    Give controllers their own subfolders

--- src/Controller/Submit/NewGenreController.php
+++ src/Controller/Submit/NewGenreController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Submit;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
--- src/Controller/Submit/NewTrackController.php
+++ src/Controller/Submit/NewTrackController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Submit;
 
 // use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
