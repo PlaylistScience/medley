87bf3ecea5fd324405854101b3f3df594e5e9578
DominicWatts <dominic.watts@hotmail.co.uk>
2019-02-04 22:08:50 +0000

    shell script to create default user

---
+++ src/Command/AddDefaultUserCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use DateTime;
+use GuzzleHttp\Client;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AddDefaultUserCommand extends Command
+{
+    const DEFAULT_EMAIL = 'default@user.co.uk';
+    const DEFAULT_PASSWORD = 'test123';
+    protected static $defaultName = 'addDefaultUserCommand';
+
+    public function __construct(UserRepository $userRepository, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->userRepository = $userRepository;
+        $this->passwordEncoder = $passwordEncoder;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Creates default user');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $io = new SymfonyStyle($input, $output);
+       
+        if($this->hasUser()) {
+            $io->success('User already created.');
+            return;
+        }
+
+        $user = new User();
+        $user->setEmail(self::DEFAULT_EMAIL);
+        $user->setRoles([]);
+        $password = $this->passwordEncoder->encodePassword($user, self::DEFAULT_PASSWORD);
+        $user->setPassword($password);
+        $this->userRepository->save($user);
+
+        $io->success('Create user successful.');
+    }
+
+    /**
+     * Check default user exists
+     * @return boolean
+     */
+    protected function hasUser()
+    {
+        if ($this->userRepository->findByEmail(self::DEFAULT_EMAIL)) {
+            return true;
+        }
+        return false;
+    }
+
+}
